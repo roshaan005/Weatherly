7d278c3f6766f9f8df0066d25d2801ec5608d602
anxious gritt <roshanjalil609@gmail.com>
2021-09-14 22:46:10 +0500

    set up the UI

---
+++ src/DOM.js
@@ -0,0 +1,83 @@
+// import cloudImage from "./images/storm.png"
+// import searchicon from "./images/search-3-16 (1).png"
+// import background from "/images/diego-jimenez-XHDtPRj535A-unsplash.jpg"
+// const backgroundPicture = document.createElement("div");
+// const pictureSection = document.createElement("div")
+// const detailsMenu = document.createElement("div");
+// const searchContainer = document.createElement("section");
+// const searchBar = document.createElement("input");
+// searchBar.setAttribute("type","text");
+// const searchBtn = document.createElement("div");
+// const searchIcon  = new Image()
+// searchIcon.src = searchicon
+// const temperatureDetails = document.createElement("section");
+// const temperatureText = document.createElement("div");
+// const weatherDescription = document.createElement("div");
+// const weatherIllustration = new Image();
+// weatherIllustration.src = cloudImage;
+// const weatherStatus = document.createElement("span");
+// const cityDescription = document.createElement("div");
+// const cityName = document.createElement("div");
+// const cityTime = document.createElement("div");
+// const details = document.createElement("section");
+// const detialsHolder = document.createElement("ul");
+// const  humidityCard = document.createElement("li");
+// const  feelsLikeCard = document.createElement("li");
+// const  sunRiseCard = document.createElement("li");
+// const  sunSetCard = document.createElement("li");
+// const  countryCard = document.createElement("li");
+// const  windSpeedCard = document.createElement("li");
+
+// // classes 
+// backgroundPicture.classList.add("background");
+// detailsMenu.classList.add("details-menu");
+// searchContainer.classList.add("search-container");
+// searchBar.classList.add("search-bar");
+// searchBtn.classList.add("search-btn");
+// pictureSection.classList.add("picture");
+// temperatureDetails.classList.add("temeperature-details");
+// temperatureText.classList.add("temeperature");
+// weatherDescription.classList.add("description");
+// weatherIllustration.classList.add("weather-illustration");
+// weatherStatus.classList.add("weather");
+// cityDescription.classList.add("city");
+// cityName.classList.add("city-name");
+// cityTime.classList.add("time");
+// details.classList.add("details");
+// detialsHolder.classList.add("details-holder");
+// humidityCard.classList.add("display-card");
+// feelsLikeCard.classList.add("display-card");
+// sunRiseCard.classList.add("display-card");
+// sunSetCard.classList.add("display-card");
+// countryCard.classList.add("display-card");
+// windSpeedCard.classList.add("display-card");
+
+// // appending to parents 
+
+
+// searchContainer.appendChild(searchBar);
+// searchBtn.appendChild(searchIcon)
+// searchContainer.appendChild(searchBtn);
+// temperatureDetails.appendChild(temperatureText);
+// weatherDescription.appendChild(weatherIllustration);
+// weatherDescription.appendChild(weatherStatus);
+// temperatureDetails.appendChild(weatherDescription);
+// cityDescription.appendChild(cityName);
+// cityDescription.appendChild(cityTime);
+// temperatureDetails.appendChild(cityDescription);
+// detialsHolder.appendChild(humidityCard);
+// detialsHolder.appendChild(feelsLikeCard);
+// detialsHolder.appendChild(sunSetCard);
+// detialsHolder.appendChild(sunRiseCard);
+// detialsHolder.appendChild(countryCard);
+// detialsHolder.appendChild(windSpeedCard);
+// details.appendChild(detialsHolder);
+// detailsMenu.appendChild(searchContainer);
+// detailsMenu.appendChild(temperatureDetails);
+// detailsMenu.appendChild(details);
+
+// backgroundPicture.style.backgroundImage = `url(${background})`
+// backgroundPicture.appendChild(pictureSection);
+// backgroundPicture.appendChild(detailsMenu);
+// const body = document.querySelector("body")
+// body.appendChild(backgroundPicture)
